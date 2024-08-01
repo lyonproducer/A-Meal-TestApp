34c28959085c3b641e312e357757a8d41f531c22
Lyon <leonardo@helgen.tech>
2024-08-01 00:37:56 -0400

    first commit

---
+++ src/app/shared/components/components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  declarations: [],
+  imports: [ CommonModule, IonicModule ],
+  exports: [],
+  providers: [],
+})
+export class ComponentsModule {}
